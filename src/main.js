991d39ba98ede6f967675a68b208695b856e431b
koki_k <kow1ag25@gmail.com>
2026-01-01 12:04:49 +0900

    アニメーション実装

--- src/main.js
+++ src/main.js
@@ -48,9 +48,80 @@ scene.add(box, torus);
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(window.devicePixelRatio);
 
+// 線形補間で滑らかに移動させる
+function lerp(x, y, a) {
+  return (1 - a) * x + a * y;
+}
+
+function scalePercent(start, end) {
+  return (scrollParcent - start) / (end - start);
+}
+
+// スクロールアニメーション
+const animationScripts = [];
+
+animationScripts.push({
+  start: 0,
+  end: 40,
+  function() {
+    camera.lookAt(box.position);
+    camera.position.set(0, 1, 10);
+    box.position.z = lerp(-15, 2, scalePercent(0, 40));
+    torus.position.z = lerp(10, -20, scalePercent(0, 40));
+  },
+});
+
+animationScripts.push({
+  start: 40,
+  end: 60,
+  function() {
+    camera.lookAt(box.position);
+    camera.position.set(0, 1, 10);
+    box.rotation.z = lerp(1, Math.PI, scalePercent(40, 60));
+  },
+});
+
+animationScripts.push({
+  start: 60,
+  end: 80,
+  function() {
+    camera.lookAt(box.position);
+    camera.position.x = lerp(0, -15, scalePercent(60, 80));
+    camera.position.y = lerp(1, -15, scalePercent(60, 80));
+    camera.position.z = lerp(10, 25, scalePercent(60, 80));
+  },
+});
+
+animationScripts.push({
+  start: 80,
+  end: 100.1,
+  function() {
+    box.rotation.x += 0.02;
+    box.rotation.y += 0.02;
+  },
+});
+
+// アニメーションを開始
+function playScrollAnimation() {
+  animationScripts.forEach((animation) => {
+    if (scrollParcent >= animation.start && scrollParcent <= animation.end)
+      animation.function();
+  });
+}
+
+// ブラウザのスクロール率を取得
+let scrollParcent = 0;
+document.body.onscroll = () => {
+  scrollParcent =
+    (document.documentElement.scrollTop /
+      (document.documentElement.scrollHeight -
+        document.documentElement.clientHeight)) *
+    100;
+};
 // アニメーション
 const tick = () => {
   window.requestAnimationFrame(tick);
+  playScrollAnimation();
   renderer.render(scene, camera);
 };
 
